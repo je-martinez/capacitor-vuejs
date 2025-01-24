bb03f2fcfc4fd6a0750b1462d73d3d19cefeac72
Jose E. Martinez <josemartinezchevez@gmail.com>
2025-01-23 21:13:49 -0600

    chore:init project

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.capacitor_vuejs_app.example',
+  appName: 'capacitor-vuejs',
+  webDir: 'dist'
+};
+
+export default config;
